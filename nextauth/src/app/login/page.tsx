6fb3d249389ec937df08a074448138d908b231de
Mostafijur Rahaman <148426873+mostafijursd@users.noreply.github.com>
2024-05-30 16:55:42 +0530

    second comment

--- nextauth/src/app/login/page.tsx
+++ nextauth/src/app/login/page.tsx
@@ -37,41 +37,54 @@ const router=useRouter();
 
     },[user])
   return (
-    <div className='bg-gradient-to-r from-slate-500 to-slate-800 flex flex-col items-center 
-    justify-center min-h-screen py-2 text-white gap-4'>
-        
-        <h1  className='text-2xl font-medium text-gray-900 dark:text-white  '>{loading ? "Processing..." : "Login"}</h1>
-        <hr />
-
-
-<label htmlFor="email"  className='text-2xl font-medium text-gray-900 dark:text-white '>email</label>
-        <input 
-        className="shadow appearance-none border rounded   text-gray-700
-         leading-tight focus:outline-none focus:shadow-outline p-2 mb-4 "
-            id="email"
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-slate-800 to-slate-700 ">
+      
+      
+     
+      <div className=" bg-gray-800 p-6 rounded-lg shadow-lg max-w-md w-full">
+        <h2 className="text-2xl font-bold mb-4 text-center text-white">{loading ? "Processing..." : "Login"}</h2>
+        <form>
+          <div className="mb-4">
+            <label className="block text-neutral-400 text-sm font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
             type="text"
             value={user.email}
             onChange={(e) => setUser({...user, email: e.target.value})}
             placeholder="email"
             />
-        <label htmlFor="password"  className='text-2xl font-medium text-gray-900 dark:text-white '>password</label>
-        <input 
-               className="shadow appearance-none border rounded   text-gray-700
-               leading-tight focus:outline-none focus:shadow-outline p-2 mb-4 "
-            id="password"
-            type="password"
-            value={user.password}
-            onChange={(e) => setUser({...user, password: e.target.value})}
-            placeholder="password"
+          </div>
+          <div className="mb-6">
+            <label className="block text-neutral-400 text-sm font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              value={user.password}
+              onChange={(e) => setUser({...user, password: e.target.value})}
+              placeholder="password"
             />
-  <button className='text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 
-  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'
-  onClick={onLogin}
- >
-    
-    {buttonDisabled ? "No Login" :"Login"}</button>
-    <Link href="/signup" className=' text-xl font-medium  dark:text-white hover:text-cyan-400 '> Visit Signup page</Link>
-        </div>
+            
+          </div>
+          <div className="flex items-center justify-between ">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              onClick={onLogin}
+            >
+              {buttonDisabled ? "No Login" :"Login"}
+            </button>
+            <Link href="/signup" className=" inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"> Visit Signup page</Link>
+            
+          </div> 
+        </form>
+      </div>
+    </div>
   )
 }
 
--- nextauth/src/app/signup/page.tsx
+++ nextauth/src/app/signup/page.tsx
@@ -37,47 +37,66 @@ const router=useRouter();
 
     },[user])
   return (
-    <div className='bg-gradient-to-r from-slate-900 to-slate-700 flex flex-col items-center 
-    justify-center min-h-screen py-2 text-white gap-3'>
-        
-        <h1 className='text-2xl font-medium text-gray-900 dark:text-white '>{loading ? "Processing..." : "Signup"}</h1>
-        <hr />
-
-        <label htmlFor="username" className='text-2xl font-medium text-gray-900 dark:text-white '>usename</label>
-<input 
-  className="p-2 border border-gray-300 rounded-lg mb-4 
-  focus:outline-none focus:border-gray-600 text-black"
-id="username"
-onChange={(e)=>setUser({...user,username:e.target.value})}
-type="text"
-placeholder='username'
-/>
-<label htmlFor="email" className='text-2xl font-medium text-gray-900 dark:text-white '>email</label>
-        <input 
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-            id="email"
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-slate-500 to-slate-800 ">
+      
+      
+     
+      <div className=" bg-gray-800 p-6 rounded-lg shadow-lg max-w-md w-full">
+        <h2 className="text-2xl font-bold mb-4 text-center text-white">{loading ? "Processing..." : "Signup"}</h2>
+        <form>
+        <div className="mb-4">
+            <label className="block text-neutral-400 text-sm font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              onChange={(e)=>setUser({...user,username:e.target.value})}
+              type="text"
+              placeholder='username'
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-neutral-400 text-sm font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
             type="text"
             value={user.email}
             onChange={(e) => setUser({...user, email: e.target.value})}
             placeholder="email"
             />
-        <label htmlFor="password"  className='text-2xl font-medium text-gray-900 dark:text-white '>password</label>
-        <input 
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-            id="password"
-            type="password"
-            value={user.password}
-            onChange={(e) => setUser({...user, password: e.target.value})}
-            placeholder="password"
+          </div>
+          <div className="mb-6">
+            <label className="block text-neutral-400 text-sm font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              value={user.password}
+              onChange={(e) => setUser({...user, password: e.target.value})}
+              placeholder="password"
             />
-  <button className='text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 
-  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'
-  onClick={onSiginup}
- >
-    
-    {buttonDisabled ? "No Signup" :"Signup"}</button>
-    <Link href="/login"  className=' text-xl font-medium  dark:text-white hover:text-cyan-400 '> Visit login page</Link>
-        </div>
+            
+          </div>
+          <div className="flex items-center justify-between ">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              onClick={onSiginup}
+            >
+              {buttonDisabled ? "No Signup" :"Signup"}
+            </button>
+            <Link href="/login" className=" inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"> Visit Login page</Link>
+            
+          </div> 
+        </form>
+      </div>
+    </div>
   )
 }
 
