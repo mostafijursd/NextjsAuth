d40bae3c3ab11b9333af37b571ed1e548053f031
Mostafijur Rahaman <148426873+mostafijursd@users.noreply.github.com>
2024-06-06 14:45:04 +0530

    second comment

--- nextauth/src/app/signup/page.tsx
+++ nextauth/src/app/signup/page.tsx
@@ -46,7 +46,7 @@ const router=useRouter();
         <form>
         <div className="mb-4">
             <label className="block text-neutral-400 text-sm font-bold mb-2" htmlFor="email">
-              Email
+              Username
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
